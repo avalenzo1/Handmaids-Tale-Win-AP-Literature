9451a9fb44b4ba692f67c1fd2ef3c5a6b831879c
Glitch (glitch-hello-website) <none>
2022-05-31 04:00:42 +0000

    💡🎱 Checkpoint ./index.html:45080849/990 ./index.js:45080849/602 ./.glitch-assets:45080849/1249 ./style.css:45080849/242

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// https://artage.io/en/icon-packs/original-windows-95-icons
+
 function init() {
   let startup = new Audio("https://cdn.glitch.global/31f9c0b6-abdb-466e-82fa-6dcaef7dfb1a/startup.mp3?v=1653963878874");
   
   startup.play();
   
-  $(".file").draggable();
+  $(".file").draggable({
+        containment: 'parent',
+        snap: "#main", snapMode: "inner",
+        snapTolerance: 3       
+    });
+  
+  $(".file").mousedown(function() {
+    $(this).focus();
+  });
 };
 
 $("#main").one("click", function() {
