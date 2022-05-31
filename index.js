b15f58699d3260297ca2a79b38aefa96709ad9b3
Glitch (glitch-hello-website) <none>
2022-05-31 03:26:22 +0000

    💷👩 Checkpoint ./style.css:45080849/636 ./index.html:45080849/151 ./index.js:45080849/270

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$("#main")
+function init() {
+  let startup = new Audio("https://cdn.glitch.global/31f9c0b6-abdb-466e-82fa-6dcaef7dfb1a/startup.mp3?v=1653963878874");
+  
+  startup.play();
+};
+
+$("#main").one("click", function() {
+  init();
+});
