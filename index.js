476610f129f1caa6250ffa9c4e74fa361a3e870d
Glitch (glitch-hello-website) <none>
2022-05-31 04:35:35 +0000

    👷🍇 Checkpoint ./index.js:45080849/4702 ./style.css:45080849/171

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
 // https://artage.io/en/icon-packs/original-windows-95-icons
 
 function init() {
-  let startup = new Audio("https://cdn.glitch.global/31f9c0b6-abdb-466e-82fa-6dcaef7dfb1a/startup.mp3?v=1653963878874")
-  
+  let startup = new Audio(
+    "https://cdn.glitch.global/31f9c0b6-abdb-466e-82fa-6dcaef7dfb1a/startup.mp3?v=1653963878874"
+  );
+
   // startup.play();
-  
+
   $(".file").draggable({
-        containment: 'parent',
-        snap: "#main", snapMode: "inner",
-        snapTolerance: 3       
-    });
-  
-  $(".file").mousedown(function() {
+    containment: "parent",
+    snap: "#main",
+    snapMode: "inner",
+    snapTolerance: 3,
+  });
+
+  $(".file").mousedown(function () {
     $(this).focus();
   });
-  
-  $(".window").mousedown(function(e) {
-    $(".selection").show();
-    $(".selection").css("top", e.clientY - $("#main").offset().top + 'px');
-    $(".selection").css("left", e.clientX - $("#main").offset().left + 'px');
-  });
-  
-  $(".window").mousemove(function(e) {
-    this.clientX = 
-    $(".selection").css("width", e.clientY - $("#main").offset().top - $("main").css("top") + 'px');
-    $(".selection").css("height", e.clientX - $("#main").offset().left - $("main").css("left") + 'px');
-  });
-  
-  $(".window").mouseup(function(e) {
-    $(".selection").hide();
-  });
-};
 
-$("#main").one("click", function() {
+  $(function () {
+    let mouseDown = false;
+
+    $(".window").mousedown(function (e) {
+      mouseDown = true;
+      $(".selection").show();
+
+      this.clientX = e.clientX - $("#main").offset().left;
+      this.clientY = e.clientY - $("#main").offset().top;
+
+      $(".selection").css("left", this.clientX + "px");
+      $(".selection").css("top", this.clientY + "px");
+    });
+
+    $(".window").mousemove(function (e) { 
+      if (mouseDown) {
+        this.clientX = e.clientX - $("#main").offset().left;
+        this.clientY = e.clientY - $("#main").offset().top;
+        this.selectionX = parseInt($(".selection").css("left"));
+        this.selectionY = parseInt($(".selection").css("top"));
+
+        $(".selection").css("width", this.clientX - this.selectionX + "px");
+        $(".selection").css("height", this.clientY - this.selectionY + "px");
+      }
+    });
+
+    $(".window").mouseup(function (e) {
+      mouseDown = false;
+
+      $(".selection").hide();
+      $(".selection").css("top", 0);
+      $(".selection").css("left", 0);
+      $(".selection").css("width", 0);
+      $(".selection").css("height", 0);
+    });
+  });
+}
+
+$("#main").one("click", function () {
   init();
 });
