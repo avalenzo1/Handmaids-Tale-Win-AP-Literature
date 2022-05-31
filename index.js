21aae692bdba7ce7ccdafdfde43c5b5d115a31bd
Glitch (glitch-hello-website) <none>
2022-05-31 03:49:30 +0000

    💍🎄 Checkpoint ./style.css:45080849/3365 ./index.html:45080849/4817 ./index.js:45080849/81

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function init() {
   let startup = new Audio("https://cdn.glitch.global/31f9c0b6-abdb-466e-82fa-6dcaef7dfb1a/startup.mp3?v=1653963878874");
   
   startup.play();
+  
+  $(".file").draggable();
 };
 
 $("#main").one("click", function() {
