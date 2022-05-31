8600e4058c40b4ca3b6f8d9831d210ee14035b8e
Glitch (glitch-hello-website) <none>
2022-05-31 23:01:59 +0000

    👦🚋 Checkpoint ./index.js:45080849/6184

--- index.js
+++ index.js
@@ -17,36 +17,38 @@ function init() {
   $(".file").mousedown(function () {
     $(this).focus();
   });
-  
-  // https://stackoverflow.com/a/20471268 SRC
-  
-  $(".context-menu").hide();
-  
-  $(".window").bind("contextmenu", function (e) {
+
+  $(function () {
+    // https://stackoverflow.com/a/20471268 SRC
+
+    $(".window").contextmenu(function (e) {
       e.preventDefault();
 
-      $(".context-menu").toggle()
+      $(".context-menu")
+        .toggle()
 
         .css({
-            top: event.pageY + "px",
-            left: event.pageX + "px"
+          top: event.pageY + "px",
+          left: event.pageX + "px",
         });
     });
 
-  $(document).bind("mousedown", function (e) {
+    $(document).bind("mousedown", function (e) {
       if (!$(e.target).parents(".context-menu").length > 0) {
-          $(".context-menu").hide();
+        $(".context-menu").hide();
       }
-  });
-  
-  $(".custom-menu li").click(function(){
+    });
+
+    $(".context-menu li").click(function () {
+      $(".context-menu").hide();
+    });
+
     $(".context-menu").hide();
   });
-  
 
   $(function () {
     let mouseDown = false;
-    
+
     $(".selection").hide();
 
     $(".bg").mousedown(function (e) {
@@ -60,35 +62,52 @@ function init() {
       $(".selection").css("top", this.clientY + "px");
     });
 
-    $(".bg").mousemove(function (e) { 
+    $(".bg").mousemove(function (e) {
       if (mouseDown) {
         this.clientX = e.clientX - $("#main")[0].getBoundingClientRect().left;
         this.clientY = e.clientY - $("#main")[0].getBoundingClientRect().top;
         this.selectionX = parseInt($(".selection").css("left"));
         this.selectionY = parseInt($(".selection").css("top"));
-        
-        
+
         if (this.clientX - this.selectionX < 0) {
-          $(".selection").css("left", this.selectionX - Math.abs(this.clientX - this.selectionX) + "px");
-          $(".selection").css("width", Math.abs(this.clientX - this.selectionX) + "px");
+          $(".selection").css(
+            "left",
+            this.selectionX - Math.abs(this.clientX - this.selectionX) + "px"
+          );
+          $(".selection").css(
+            "width",
+            Math.abs(this.clientX - this.selectionX) + "px"
+          );
         } else {
           $(".selection").css("left", this.selectionX + "px");
-          $(".selection").css("width", Math.abs(this.clientX - this.selectionX) + "px");
+          $(".selection").css(
+            "width",
+            Math.abs(this.clientX - this.selectionX) + "px"
+          );
         }
-        
+
         if (this.clientY - this.selectionY < 0) {
-          $(".selection").css("top", this.selectionY - Math.abs(this.clientY - this.selectionY) + "px");
-          $(".selection").css("height", Math.abs(this.clientY - this.selectionY) + "px");  
+          $(".selection").css(
+            "top",
+            this.selectionY - Math.abs(this.clientY - this.selectionY) + "px"
+          );
+          $(".selection").css(
+            "height",
+            Math.abs(this.clientY - this.selectionY) + "px"
+          );
         } else {
           $(".selection").css("top", this.selectionY + "px");
-          $(".selection").css("height", Math.abs(this.clientY - this.selectionY) + "px");  
+          $(".selection").css(
+            "height",
+            Math.abs(this.clientY - this.selectionY) + "px"
+          );
         }
       }
     });
 
     $(".bg").mouseup(function (e) {
       mouseDown = false;
-      
+
       $(".selection").removeAttr("style");
       $(".selection").hide();
     });
