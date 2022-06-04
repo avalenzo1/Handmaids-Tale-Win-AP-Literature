daa5fd8ebc1b863c7413dc7471964b09228418ac
Glitch (glitch-hello-website) <none>
2022-06-04 03:22:17 +0000

    🍚💴 Checkpoint ./style.css:45080849/126 ./os.js:45080849/89

--- os.js
+++ os.js
@@ -98,7 +98,7 @@ class Window {
   }
 }
 
-$(".window-prompt").click(function() {
+$("window-prompt").focus(function() {
   console.log("Sdf")
   $(".window-prompt").removeClass("active");
   $(this).addClass("active");
